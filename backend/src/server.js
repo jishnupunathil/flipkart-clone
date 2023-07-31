bc67dc1f8f41172b247715774e39d318d73ded1f
jishnupunathil <jishnupunathil000@gmail.com>
2023-07-31 17:11:43 +0530

    first signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const env=require('dotenv');
+const bodyParser=require('body-parser');
 const app=express();
+const mongoose = require('mongoose');
 
+//routes
+
+const userRoutes=require('./routes/user')
 
 //environment variable
 env.config()
 
-app.get('/' ,(req,res)=>{
+app.use(bodyParser.json());
+app.use('/api',userRoutes);
 
-    res.status(200).json({
-        message:'Hello World'
-    })
+//mongodb connection
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.qkkqehk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log('Database Connected');
+});
 
-})
+
+
 
 
 app.listen(process.env.PORT ,()=>{
